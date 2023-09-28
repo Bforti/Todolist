f19ac44ec00922fc7d4b1e58adc8d742e3011666
Bforti <139017911+Bforti@users.noreply.github.com>
2023-09-28 03:13:37 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+let initial={
+	label:"",
+	done:false
+}
 const Home = () => {
+	const [initialTarea, setInitialTarea]=useState(initial)
+	const[tareaCulminada, setTareaCulminada]=useState([])
+	const[error, setError]=useState(false)
+
+	const agregandoTareas =(event)=>{
+		setInitialTarea({
+			label:event.target.value,
+			done:false
+		})
+		}
+	const guardandotareas=(event)=>{
+		console.log(event.key)
+		if(event.key=="Enter"){
+			setTareaCulminada([...tareaCulminada, initialTarea])
+            setInitialTarea(initial)
+		}
+		
+	}
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<input type="text"
+		    placeholder="ingresa tu tarea"
+		    name="label"
+		    value={initialTarea.label} 
+		    onChange={agregandoTareas}
+			onKeyDown={guardandotareas}
+		 />
+		 <ul>
+			{tareaCulminada.map((item,index)=>{
+				return(
+					<li key={index}>
+						{item.label}
+					</li>	
+
+
+				)
+			})
+			}
+
+		</ul>
 		</div>
 	);
 };
